b54954d1e22e68711447b227678fe4ab26cd792a
Cara Dulay <dulaycara@gmail.com>
2024-09-14 00:00:53 +0800

    feat: added form view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Position;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $positions=Position::with('position')->get();
+        return view('home',compact('positions'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,32 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="card align-middle justify-content-center align-items-center">
+                <div class="modal-body m-4">
+                    <form action="{{ route('create/position') }}" method="POST">
+                        @csrf
+                            <div class="m-2 col-12 col-sm-12">
+                                <div class="form-group">
+                                    <label>Name:</label>
+                                    <input type="text" class="form-control" name="name" required>
+                                </div>
+                            </div>
+                            <div class="m-2 col-12 col-sm-12">
+                                <div class="form-group">
+                                    <label>Reports To:</label>
+                                    <select class="form-control" id="reports_to" name="reports_to">
+                                        <option value="">-- Select Position --</option>
+                                        @foreach($positions as $position)
+                                            <option value="{{ $position->id }}">{{ $position->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="m-2 mb-0 pt-3 pb-3 text-center">
+                                <button class="btn btn-success btn-block" type="submit">Submit</button>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </form>
                 </div>
             </div>
         </div>
